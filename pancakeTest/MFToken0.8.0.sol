dc235479db5dad394875a2b81a60282c2fc91a0d
YanLong-111 <2944972991@qq.com>
2022-06-16 14:22:15 +0800

    add testPool.sol

--- pancakeTest/MFToken0.8.0.sol
+++ pancakeTest/MFToken0.8.0.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MF", "MF") {
+    constructor(uint256 initialSupply) ERC20("MF1", "MF1") {
         _mint(msg.sender, initialSupply);
     }
 }
---
+++ pancakeTest/testPool.sol
@@ -0,0 +1,237 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+/// @title Lp dividend contract
+/// @author Long
+/// @notice This contract is only for lp dividend operation
+contract lpDividend is ReentrancyGuard, Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+    /* ========== EVENTS ========== */
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+
+    /* ========== STATE VARIABLES ========== */
+
+    /// @dev tokens address
+    IERC20 public tokenContract;
+    IERC20 public rewardsToken;
+    IERC20 public stakingToken;
+
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+
+    mapping(address => bool) public blacklist;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    /* ========== VIEWS ========== */
+
+    /**
+    * @dev Total pledge amount
+    * @return _totalSupply
+    */
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+    * @dev User pledge amount
+    * @param account User address
+    * @return User pledge amount
+    */
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+    * @dev Determine if the end time is reached
+    * @return The current time and the end time are smaller
+    */
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    /*
+    * @dev Amount of rewards earned per staked token
+    * @return Number of awards
+    */
+    function rewardPerToken() public view returns (uint256) {
+        if (_totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+        rewardPerTokenStored.add(
+            lastTimeRewardApplicable().sub(lastUpdateTime).mul(rewardRate).mul(1e18).div(_totalSupply)
+        );
+    }
+
+    /**
+    * @dev Query the number of rewards a user has received
+    * @param account User address
+    * @return The number of rewards the user has earned
+    */
+    function earned(address account) public view returns (uint256) {
+        return _balances[account].mul(rewardPerToken().sub(userRewardPerTokenPaid[account])).div(1e18).add(rewards[account]);
+    }
+
+    /**
+    * @dev Returns all addresses used for verification
+    * @return addressList Token address List
+    */
+    function getTokenAddr() external view returns (address[3] memory addressList){
+        addressList[0] = address(tokenContract);
+        addressList[1] = address(rewardsToken);
+        addressList[2] = address(stakingToken);
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    /**
+    * @dev User pledges tokens
+    * @param amount User pledge amount
+    */
+    function stake(uint256 amount) external nonReentrant updateReward(_msgSender()) {
+        require(amount > 0, "Cannot stake 0");
+        require(!blacklist[_msgSender()], "is blacklist");
+        _totalSupply = _totalSupply.add(amount);
+        _balances[_msgSender()] = _balances[_msgSender()].add(amount);
+        stakingToken.safeTransferFrom(_msgSender(), address(this), amount);
+        emit Staked(_msgSender(), amount);
+    }
+
+    /**
+    * @dev User redeems principal
+    * @param amount The amount of principal redeemed by the user
+    */
+    function withdraw(uint256 amount) public nonReentrant updateReward(_msgSender()) {
+        require(amount > 0, "Cannot withdraw 0");
+        require(!blacklist[_msgSender()], "is blacklist");
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[_msgSender()] = _balances[_msgSender()].sub(amount);
+        stakingToken.safeTransfer(_msgSender(), amount);
+        emit Withdrawn(_msgSender(), amount);
+    }
+
+    /**
+    * @dev Users receive rewards
+    */
+    function getReward() public nonReentrant updateReward(_msgSender()) {
+        require(!blacklist[_msgSender()], "is blacklist");
+        uint256 reward = rewards[_msgSender()];
+        if (reward > 0) {
+            rewards[_msgSender()] = 0;
+            rewardsToken.safeTransfer(_msgSender(), reward);
+            emit RewardPaid(_msgSender(), reward);
+        }
+    }
+
+    /**
+    * @dev User logout(withdraw and getReward)
+    */
+    function exit() external {
+        withdraw(_balances[_msgSender()]);
+        getReward();
+    }
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    /**
+    * @dev Set user  blacklist
+    * @param userAddrs User address array
+    * @param condition User status
+    *         true:add to blacklist
+    *         false:Cancel from blacklist
+    */
+    function setBlacklist(address[] calldata userAddrs, bool condition) public onlyOwner {
+        for (uint256 i; i < userAddrs.length; i++) {
+            blacklist[userAddrs[i]] = condition;
+        }
+    }
+
+    /**
+    * @dev Set the number of rewards per second
+    * @param reward New number of rewards per second
+    * @param timestamp New reward Duration
+    */
+    function notifyRewardAmount(uint256 reward, uint256 timestamp) external updateReward(address(0)) {
+        require(address(tokenContract) == _msgSender() || owner() == _msgSender(), "STC:not allowed");
+        rewardRate = reward;
+
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp.add(timestamp);
+        emit RewardAdded(reward);
+    }
+
+    /**
+    * @dev Set the token address(token contract use once!!!)
+    * @param usdtAddr New Reward Token Address
+    * @param tokenAddr New pledge token address
+    */
+    function setRewardsToken(IERC20 usdtAddr, IERC20 tokenAddr) external {
+        require(address(tokenContract) == _msgSender() || owner() == _msgSender(), "SRT:not allowed");
+        rewardsToken = usdtAddr;
+        stakingToken = tokenAddr;
+    }
+
+    /**
+    * @dev Set the token contract address
+    * @param _token Token contract address
+    */
+    function setWerewolfKillToken(IERC20 _token) public onlyOwner {
+        tokenContract = _token;
+        //_token.safeApprove(address(_token), ~uint(0));
+    }
+
+    // /**
+    // * @dev transfer all token token
+    // * @param contractAddr Payment contract address
+    // */
+    // function transferToken(address contractAddr) external returns (uint256 balance) {
+    //     require(address(tokenContract) == _msgSender(), "TT:not allowed");
+    //     balance = tokenContract.balanceOf(address(this));
+    //     tokenContract.safeTransfer(contractAddr, balance);
+    // }
+
+    function claimTokens(
+        address tokenAddress,
+        address to,
+        uint256 amount
+    ) public onlyOwner {
+        if (amount > 0) {
+            if (tokenAddress == address(0)) {
+                payable(to).transfer(amount);
+            } else {
+                IERC20(tokenAddress).safeTransfer(to, amount);
+            }
+        }
+    }
+
+    /* ========== MODIFIERS ========== */
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+}
